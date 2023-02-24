7c71a92b7c7df089a39ebb19f676f9d62817057b
Greenvarc <77793020+Greenvarc@users.noreply.github.com>
2023-02-24 06:39:45 +0000

    first try

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include<iostream>
-#using namespace std
-int main(){
-  cout<<"hello bro"<<endl;
+#include <iostream>
+using namespace std;
+int main()
+{
+  string name;
+  cout << "what \' your name ?" << endl;
+  cin >> name;
+  cout << "Hello " << name << " nice to meet you " << endl;
   return 0;
 }
